860cc0a3436b830aee48e591381af4839ba9ca71
wangxu43 <wangxu43@58.com>
2020-08-12 21:34:02 +0800

    Most Water2

--- Week_01/MostWater.java
+++ Week_01/MostWater.java
@@ -4,6 +4,7 @@ public class MostWater {
         int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println(maxArea1(arr));
         System.out.println(maxArea2(arr));
+        System.out.println(maxArea3(arr));
     }
 
 
@@ -23,6 +24,22 @@ public class MostWater {
         for (int i = 0, j = height.length - 1; i < j; ) {
             int minHeight = height[i] < height[j] ? height[i++] : height[j--];
             maxArea = Math.max(maxArea, (j - i + 1) * minHeight);
+        }
+        return maxArea;
+    }
+
+    public static int maxArea3(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                maxArea = Math.max(maxArea, height[left] * (right - left));
+                left++;
+            }else{
+                maxArea = Math.max(maxArea, height[right] * (right - left));
+                right--;
+            }
         }
         return maxArea;
     }
