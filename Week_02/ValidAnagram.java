bd8c38895da87457dada90b65fca8df977ec7864
wangxu43 <wangxu43@58.com>
2020-08-17 22:05:54 +0800

    valid anagram

---
+++ Week_02/ValidAnagram.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+/**
+ * 有效的字母异位词
+ * valid-anagram
+ * https://leetcode-cn.com/problems/valid-anagram/
+ */
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram1(s, t));
+        System.out.println(isAnagram2(s, t));
+    }
+
+    public static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] char1 = s.toCharArray();
+        char[] char2 = t.toCharArray();
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+        return Arrays.equals(char1, char2);
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
