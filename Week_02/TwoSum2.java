b73e9d117e2a9ba3e34f93bd1bc4f67e1ae38cdd
wangxu43 <wangxu43@58.com>
2020-08-19 10:50:05 +0800

    two sum

--- Week_02/TwoSum2.java
+++ Week_02/TwoSum2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,10 @@ import java.util.Map;
  */
 public class TwoSum2 {
 
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 7};
+        int[] ints = twoSum4(arr, 6);
+    }
 
     /**
      * 暴力求解
@@ -15,7 +20,7 @@ public class TwoSum2 {
      * @param target
      * @return
      */
-    public int[] twoSum1(int[] nums, int target) {
+    public static int[] twoSum1(int[] nums, int target) {
         int[] arr = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -35,7 +40,7 @@ public class TwoSum2 {
      * @param target
      * @return
      */
-    public int[] twoSum2(int[] nums, int target) {
+    public static int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
@@ -54,7 +59,7 @@ public class TwoSum2 {
      * @param target
      * @return
      */
-    public int[] twoSum3(int[] nums, int target) {
+    public static int[] twoSum3(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
                 int content = target - nums[i];
@@ -64,6 +69,40 @@ public class TwoSum2 {
                 map.put(nums[i], i);
             }
             return new int[]{};
+    }
+
+    /**
+     * 两头夹逼法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum4(int[] nums, int target) {
+        int[] ints = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums);
+        int x = 0;
+        int y = 0;
+        for (int i = 0, j = nums.length - 1; i < nums.length;) {
+            if (nums[i] + nums[j] == target) {
+                x = nums[i];
+                y = nums[j];
+                break;
+            } else if (nums[i] + nums[j] > target) {
+                j--;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            }
+        }
+        int a = -1;
+        int b = -1;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == x && a == -1) {
+                a = i;
+            }else if (ints[i] == y && b == -1) {
+                b = i;
+            }
+        }
+        return a == -1 || b == -1 ? new int[]{} : new int[]{a, b};
     }
 
 }
