de509e5784c21147f80f66c95444ae5adab82071
wangxu43 <wangxu43@58.com>
2020-08-25 11:09:19 +0800

    最小k个数

--- Week_02/LeastNumbers.java
+++ Week_02/LeastNumbers.java
@@ -33,4 +33,28 @@ public class LeastNumbers {
         }
         return ret;
     }
+
+
+    public static int[] getLeastNumbers2(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        if (k == arr.length) {
+            return arr;
+        }
+        Queue<Integer> priorityQueue = new PriorityQueue<>((v1, v2) -> v2 - v1);
+        for (int i = 0; i < arr.length; i++) {
+            if (priorityQueue.size() < k) {
+                priorityQueue.add(arr[i]);
+            }else if (priorityQueue.peek() > arr[i]) {
+                priorityQueue.poll();
+                priorityQueue.add(arr[i]);
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; i++) {
+            ret[i] = priorityQueue.poll();
+        }
+        return ret;
+    }
 }
