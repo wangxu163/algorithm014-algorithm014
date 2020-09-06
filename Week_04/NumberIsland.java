6616fdc3fb931e72dc47f456abbf7f25e74c5c2b
wangxu43 <wangxu43@58.com>
2020-09-06 23:19:17 +0800

    number island dfs

--- Week_04/NumberIsland.java
+++ Week_04/NumberIsland.java
@@ -5,7 +5,35 @@
 public class NumberIsland {
 
     public int numIslands(char[][] grid) {
-
-        return 0;
+        if (null == grid || grid.length < 0) {
+            return 0;
+        }
+        int nr = grid.length;//行
+        int nc = grid[0].length;//列
+        int num_island = 0;
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == 1) {
+                    ++num_island;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return num_island;
     }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+
+
 }
