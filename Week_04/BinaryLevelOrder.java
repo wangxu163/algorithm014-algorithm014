d3d1c39c226eb161347118e01e8334e2a9b469d0
wangxu43 <wangxu43@58.com>
2020-09-06 21:37:02 +0800

    binary level order

---
+++ Week_04/BinaryLevelOrder.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/#/description
+ */
+public class BinaryLevelOrder {
+
+    /**
+     * BFS
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> solutions = new ArrayList<>();
+        if (null == root) {
+            return solutions;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                list.add(poll.val);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            solutions.add(list);
+        }
+        return solutions;
+    }
+}
