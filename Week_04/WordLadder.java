cb0e6c0ac510ad41430802a3786ea604c47273c1
wangxu43 <wangxu43@58.com>
2020-09-06 23:42:18 +0800

    word ladder

--- Week_04/WordLadder.java
+++ Week_04/WordLadder.java
@@ -1,4 +1,6 @@
-import java.util.List;
+import javafx.util.Pair;
+
+import java.util.*;
 
 /**
  * 127. 单词接龙
@@ -7,7 +9,38 @@ import java.util.List;
 public class WordLadder {
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int l = beginWord.length();
+        Map<String, List<String>> allComboDict = new HashMap<>();
+        wordList.forEach(word->{
+            for (int i = 0; i < l; i++) {
+                String newWord = word.substring(0, i) + "*" + word.substring(i + 1, l);
+                List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
+                transformations.add(word);
+                allComboDict.put(newWord, transformations);
+            }
+        });
+        Queue<Pair<String, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(beginWord, 1));
 
+        Map<String, Boolean> visited = new HashMap<>();
+        visited.put(beginWord, true);
+        while (!queue.isEmpty()) {
+            Pair<String, Integer> node = queue.poll();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < l; i++) {
+                String newWord = word.substring(0, i) + "*" + word.substring(i + 1, l);
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        queue.add(new Pair<>(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
         return 0;
     }
 }
