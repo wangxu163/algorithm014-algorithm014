a4a164fc7421a11b7db99cf19386fe765c4b8b87
wangxu43 <wangxu43@58.com>
2020-09-07 08:53:24 +0800

    word ladder

--- Week_04/WordLadder.java
+++ Week_04/WordLadder.java
@@ -43,4 +43,38 @@ public class WordLadder {
         }
         return 0;
     }
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        int l = beginWord.length();
+        Map<String, List<String>> allwordMap = new HashMap<>();
+        wordList.forEach(word->{
+            for (int i = 0; i < l; i++) {
+                String newWord = word.substring(0, i) + "*" + word.substring(i + 1, l);
+                List<String> orDefault = allwordMap.getOrDefault(newWord, new ArrayList<>());
+                orDefault.add(word);
+                allwordMap.put(newWord, orDefault);
+            }
+        });
+        Queue<Pair<String, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair(beginWord, 1));
+        Map<String, Boolean> visited = new HashMap<>();
+        visited.put(beginWord, true);
+        while (!queue.isEmpty()) {
+            Pair<String, Integer> poll = queue.poll();
+            String word = poll.getKey();
+            Integer level = poll.getValue();
+            for (int i = 0; i < l; i++) {
+                String newWord = word.substring(0, i) + "*" + word.substring(i + 1, l);
+                for (String adjacentWord : allwordMap.getOrDefault(newWord, new ArrayList<>())) {
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        queue.add(new Pair<>(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 }
